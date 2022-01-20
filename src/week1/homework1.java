a3cc954489bb7fbd510ae15c193309b0ce258843
Christina Dietrich <christina.dietrich@my.simpson.edu>
2022-01-20 12:45:17 -0600

    Work on homework 2

--- src/week1/homework1.java
+++ src/week1/homework1.java
@@ -89,5 +89,25 @@ public class homework1 {
 //        }
 //
 //    }
+//public static void main(String[] args) {
+//    int x = 1;
+//    float y = 1;
+//    System.out.println(x = x + y);
+//    System.out.println(x += y);
+//}
 
+//    public static void main(String[] args) {
+//        System.out.println("Celsius | Fahrenheit");
+//        System.out.println("________|___________");
+//        double tempinf;
+//        for (int celsius = 0; celsius <= 100; celsius += 10)
+//        {
+//            System.out.println("     " + celsius + "  |     " + (tempinf = (celsius * (9.0/5)) + 32));
+//        }
+//    }
+
+    public static void main(String[] args) {
+        for (int count = 817; count > 10; count-= 2)
+            System.out.print(count + " ");
+    }
 }
