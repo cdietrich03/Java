306393a19daa9f7716ef0b9b17ffb937cbb6915c
Christina Dietrich <christina.dietrich@my.simpson.edu>
2022-01-26 10:06:14 -0600

    Work on notes 3

--- src/week2/homework2.java
+++ src/week2/homework2.java
@@ -1,6 +1,8 @@
 package week2;
 
 import java.text.DecimalFormat;
+import java.util.Scanner;
+
 
 public class homework2 {
     //    public static void main(String[] args) {
@@ -18,16 +20,56 @@ public class homework2 {
 //            System.out.print(count + " ");
 //    }
 
-    public static void main(String[] args) {
-        double total = 0;
-        double distance = 10;
-        int day = 0;
-        DecimalFormat formatter1 = new DecimalFormat("#0.000");
-        do {
-            day += 1;
-            total += distance;
-            System.out.println("Day " + day + "  distance today = " + formatter1.format(distance) + " total = " + formatter1.format(total));
-            distance *= 0.7;
-        }while (total > 0.1);
-}
+//    public static void main(String[] args) {
+//        double total = 0;
+//        double distance = 10;
+//        int day = 0;
+//        DecimalFormat formatter1 = new DecimalFormat("#0.000");
+//        do {
+//            day += 1;
+//            total += distance;
+//            System.out.println("Day " + day + "  distance today = " + formatter1.format(distance) + " total = " + formatter1.format(total));
+//            distance *= 0.7;
+//        }while (total > 0.1);
+//}
+//public static void main(String[] args) {
+//    Scanner input = new Scanner(System.in);
+//    System.out.print("Enter a letter grade: ");
+//    String grade = input.next();
+//    int gradeValue = 0;
+//
+//    switch(grade) {
+//        case "A":
+//            System.out.println("A is worth 4");
+//            gradeValue = 4;
+//            break;
+//        case "B":
+//            System.out.println("B is worth 3");
+//            gradeValue = 3;
+//            break;
+//        case "C":
+//            System.out.println("C is worth 2");
+//            gradeValue = 2;
+//            break;
+//        case "D":
+//            System.out.println("D is worth 1");
+//            gradeValue = 1;
+//            break;
+//        case "F":
+//            System.out.println("F is worth 0");
+//            gradeValue = 0;
+//            break;
+//        default:
+//            System.out.println("Invalid choice.");
+//    }
+//}
+//public static void main(String[] args) {
+//    for (int count = 1; count < 10; count++)
+//    {
+//        System.out.print(count);
+//        if (count > 5) count = 10;
+//    }
+//    System.out.println();
+//
+//}
 }
--- src/week3/notes.java
+++ src/week3/notes.java
@@ -27,20 +27,71 @@ public class notes {
 //
 //    }
 //
-public static void main(String[] args) {
-    String s1 = "Monty";
-    String s2 = "Python";
+//public static void main(String[] args) {
+//    String s1 = "Monty";
+//    String s2 = "Python";
+//
+//    System.out.println(s1.length() + s2.length());
+//    System.out.println(s2.charAt(4));
+//    System.out.println(s1.charAt(2) == s2.charAt(s2.length() - 1));
+//    System.out.println(s1.substring(0, 2) + s2.substring(4, 6));
+//    System.out.println(s1.contains("ty") && s2.contains("yt"));
+//    System.out.println("hello".indexOf("l"));
+//    System.out.println(s1.indexOf("on") + s2.indexOf("a"));
+//    System.out.println((s1 + s2).replace("o", "i"));
+//    System.out.println("remove all the e's from this text".replace("e", ""));
+//
+//
+//}
+//    public static String singFullSong(String name, int age) {
+//        String mainClause = "Happy birthday to you";
+//        String out = "Happy birthday to you, happy birthday to you";
+//        out += " Happy birthday dear " + name;
+//        out += " Happy birthday to you";
+//        return out;
+//    }
+//
+//    public static void singHappyBirthday(String name, int age){
+//        System.out.println("Happy Birthday " + name);
+//        System.out.println("You are " + age + " years old");
+//        System.out.println(singFullSong("Suzy", 6));
+//
+//    }
+//    public static void main(String[] args) {
+//        System.out.println("Starting main");
+//        singHappyBirthday("Suzy", 6);
+//        System.out.println("Back in main");
+//}
+//public static boolean badFunction (int int1) {
+//    if (int1 > 0) return true;
+//    else if (int1 < 0) return false;
+////    else if (int1 == 0) return true;
+//    else return true;
+//}
 
-    System.out.println(s1.length() + s2.length());
-    System.out.println(s2.charAt(4));
-    System.out.println(s1.charAt(2) == s2.charAt(s2.length() - 1));
-    System.out.println(s1.substring(0, 2) + s2.substring(4, 6));
-    System.out.println(s1.contains("ty") && s2.contains("yt"));
-    System.out.println("hello".indexOf("l"));
-    System.out.println(s1.indexOf("on") + s2.indexOf("a"));
-    System.out.println((s1 + s2).replace("o", "i"));
-    System.out.println("remove all the e's from this text".replace("e", ""));
+//    public static boolean isPrime(int number) {
+//
+//        boolean divisorFound = false;
+//
+//        for (int divisor = 2; divisor <= Math.sqrt(number); divisor ++)
+//            if (number % divisor == 0) divisorFound = true;
+//
+//        return !divisorFound;
+//    }
 
+    public static boolean isOdd (int number) {
+        if (number % 2 != 0) {
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        if (isOdd(5)) {
+            System.out.println("it is odd");
+        }
+    }
 
 }
-}
+
