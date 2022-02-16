ec752c2027e1e6a41d524aafd93f512ad86034c2
Christina Dietrich <christina.dietrich@my.simpson.edu>
2022-02-16 09:07:02 -0600

    work on hw5

---
+++ src/week5/homework6.java
@@ -0,0 +1,39 @@
+package week5;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+
+public class homework6 {
+    public static void main(String[] args) {
+//
+//        int[] intArray = {4, 1, 3};
+//        String[] stringArray = {"hi", "bye", "mymymy!"};
+//        double[] doubleArray = new double[5];
+//
+//        doubleArray[0] = intArray[1];
+//        doubleArray[1] = 1.5;
+//
+//        int[] otherIntArray = intArray;
+//        int[] anotherIntArray = Arrays.copyOf(intArray, intArray.length);
+//
+//        boolean arraysEqualOne = (anotherIntArray == intArray);
+//        boolean arraysEqualTwo = (Arrays.equals(anotherIntArray, intArray));
+//        intArray[1] += intArray[2];
+//
+//        System.out.println(arraysEqualTwo);
+
+        ArrayList<Double> list1 = new ArrayList<>();
+        list1.add(1.1);
+        list1.add(2.2);
+        list1.add(3.3);
+        list1.add(4.4);
+        ArrayList<Double> list2 = new ArrayList<>();
+        list2.add(5.5);
+        list2.add(6.6);
+        (list2, list1);
+        System.out.println(list2);
+
+
+    }
+}
